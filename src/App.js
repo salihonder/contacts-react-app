3d7bffdf524beaa082d8c8b9562a67c15c99d697
Salih Onder <salihonderx@gmail.com>
2019-01-27 02:13:10 -0500

    feat: Create ContactList component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ContactList extends Component {
+  render(){
+    const persons = [
+      {name: 'Salih'},
+      {name: 'Semih'},
+      {name: 'Sinan'}
+    ];
+
+    return <ol>
+      {persons.map(person => (
+          <li key={person.name}>{person.name}</li>
+        )
+      )}
+      
+    </ol>
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" alt="logo" />     
+            <ContactList /> 
         </header>
       </div>
     );
